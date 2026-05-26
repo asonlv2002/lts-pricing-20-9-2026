b0231f9c77c2190a059af79bd25b8caa9241ae66
asonlv2002 <ason.vohuynhson@gmail.com>
2026-05-26 19:15:26 +0700

    ok

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
   //transpilePackages: ['@lts/bang-tinh-gia', '@lts/hang-so', '@lts/kieu-du-lieu'],
 };
 
